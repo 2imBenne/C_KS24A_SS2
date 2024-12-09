765a2d714e617d084d41e858118dcf5841302ef6
huylg <lamgiahuy002203@gmail.com>
2024-12-09 23:41:17 +0700

    hoàn thành bt6

---
+++ Session2-bt6.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	
+	int r;	scanf("%d",&r);
+	const float PI = 3.14;
+	float P =  PI * (r * r);
+	float S = 2 * PI * r; 
+	printf("Chu vi hinh tron la: %f \n",P);
+	printf("Dien tich hinh tron la: %f \n",S);	 
+} 
