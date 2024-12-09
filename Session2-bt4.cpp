06541c70e3df783e5668f37bdccd3f8e14125ece
huylg <lamgiahuy002203@gmail.com>
2024-12-09 23:14:53 +0700

    hoàn thành bt4

---
+++ Session2-bt4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	
+	int canh;	scanf("%d",&canh);
+	int P = canh * 4; 
+	printf("Chu vi hinh vuong la: %d \n",P);
+	int S = canh * canh;
+	printf("Dien tich hinh vuong la: %d \n",S); 
+	 
+} 
