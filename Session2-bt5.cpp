e946a31fd59bfcbe2297eaf1656ac3c990ccf073
huylg <lamgiahuy002203@gmail.com>
2024-12-09 23:20:24 +0700

    Hoàn thành bt5

---
+++ Session2-bt5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	
+	int dai,rong;	scanf("%d%d",&dai,&rong);
+	int P = (dai + rong)*2; 
+	printf("Chu vi hinh chu nhat la: %d \n",P);
+	int S = dai * rong;
+	printf("Dien tich hinh chu nhat la: %d \n",S); 
+	 
+} 
